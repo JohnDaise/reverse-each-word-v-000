ed9d69b2e45f7532c8300a6b0f270336b4c19435
John Daise <jdaise@gmail.com>
2018-01-09 00:29:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
+  a = " "
   sentence = "How are you?"
   sentence.split(" ").collect do |element|
     element.reverse!
+      a<<element
   end
-  a = " "
-  a<<element
 end
